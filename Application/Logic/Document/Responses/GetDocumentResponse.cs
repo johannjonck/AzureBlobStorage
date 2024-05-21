3f86eedf66a52b49fbf9b6232884ce0ba342633d
Johann Jonck <JJonck@iconsa.co.za>
2024-05-21 10:52:01 +0200

    Code refactor

--- Application/Logic/Document/Responses/GetDocumentResponse.cs
+++ Application/Logic/Document/Responses/GetDocumentResponse.cs
@@ -4,7 +4,7 @@ namespace Application.Logic.Document.Responses
 {
     public class GetDocumentResponse //TODO: Use a generic response -> combine Add, Get and List ???
     {
-        public DocumentDto Document { get; set; }
+        public DocumentDto? Document { get; set; }
 
     }
 }
