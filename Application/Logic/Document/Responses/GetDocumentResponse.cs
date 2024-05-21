8fe2b4a40513856da57cf850cae42b2bc310ee2c
Johann Jonck <JJonck@iconsa.co.za>
2024-05-21 10:58:57 +0200

    Code cleanup

--- Application/Logic/Document/Responses/GetDocumentResponse.cs
+++ Application/Logic/Document/Responses/GetDocumentResponse.cs
@@ -2,7 +2,7 @@
 
 namespace Application.Logic.Document.Responses
 {
-    public class GetDocumentResponse //TODO: Use a generic response -> combine Add, Get and List ???
+    public class GetDocumentResponse 
     {
         public DocumentDto? Document { get; set; }
 
